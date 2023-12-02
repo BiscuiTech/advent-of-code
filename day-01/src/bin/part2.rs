290c8ad2ec74f7540825dc2e2fe45a9a04845c8b
BiscuiTech <jhuet@biscui.tech>
2023-12-02 12:32:47 -0500

    day 1

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,27 +1,42 @@
 fn main() {
     let input = include_str!("./input1.txt");
     let output = part2(input);
-    // dbg!(ouput)
+    dbg!(output);
 }
 
-fn part2(input: &str) -> u32 {
-    let string_numbers = vec![
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-    ];
+const STRING_NUMBERS: [(&str, u8); 9] = [
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
 
-    string_numbers
-        .into_iter()
-        .map(|s| input.replace(s.0, s.1.to_string().as_str()));
-    dbg!(input);
-    todo!()
+fn part2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut payload = line.chars().enumerate().filter_map(|(index, char)| {
+                if let Some(digit) = char.to_digit(10) {
+                    Some(digit)
+                } else {
+                    let sub_str = &line[index..];
+                    STRING_NUMBERS.iter().find_map(|(digit_as_string, digit)| {
+                        sub_str
+                            .starts_with(digit_as_string)
+                            .then_some(*digit as u32)
+                    })
+                }
+            });
+            let first = payload.next().expect("Not found");
+            let last = payload.last().unwrap_or(first);
+            first * 10 + last
+        })
+        .sum::<u32>()
 }
 
 #[cfg(test)]
